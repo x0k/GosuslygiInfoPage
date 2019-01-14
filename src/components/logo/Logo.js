08aa394926f2d3c8a7c5d0eb5c3cde55e8bf3114
Roman <x0kill@ya.ru>
2019-01-15 01:29:28 +0300

    Update logo component

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,30 +1,18 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import './Logo.css';
 
-class Logo extends Component {
-  render() {
-    let className = 'logo';
-    if (this.props.className) {
-      className += ` ${this.props.className}`;
-    }
-    return (
-      <div className={className}>
-        <img src={this.props.src} alt="logo" className="logo__img" />
-        <div className="logo__data">
-          <div className="logo__title">{this.props.title}</div>
-          <div className="logo__subtitle">{this.props.subtitle}</div>
-        </div>
-      </div>
-    );
+export default function Logo ({ className, src, title, subtitle }) {
+  let classNames = 'logo';
+  if (className) {
+    classNames += ` ${className}`;
   }
+  return (
+    <div className={classNames}>
+      <img src={src} alt="logo" className="logo__img" />
+      <div className="logo__data">
+        <div className="logo__title">{title}</div>
+        <div className="logo__subtitle">{subtitle}</div>
+      </div>
+    </div>
+  );
 }
-
-Logo.propTypes = {
-  className: PropTypes.string,
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
-  src: PropTypes.any.isRequired
-};
-
-export default Logo;
